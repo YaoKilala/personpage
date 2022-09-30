d68d6afe1afbc0658f516a8b661cde520b827949
YaoKilala <108784111+YaoKilala@users.noreply.github.com>
2022-09-30 10:09:08 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -4,59 +4,73 @@ function muXie() {
 }//打开页面 木屑配方
 
 function index() {
-    window.location.href = ("./index.html")
+    top.location.replace("./index.html");
 }//页面跳转 首页
 
 function index_breed() {
-    window.location.href = ("./index_breed.html")
+    top.location.replace("./index_breed.html")
 }//页面跳转 昆虫饲养
 
 function index_drawing() {
-    window.location.href = ("./index_drawing.html")
+    top.location.replace("./index_drawing.html")
 }//页面跳转 绘画作品
 
-function index_tujian() {
-    window.location.href = ("./index_tujian.html")
-}//页面跳转 昆虫图鉴
+function tuJian_insect_qiaoChiMu() {
+    top.location.replace("./tuJian_insect_qiaoChiMu.html")
+}//页面跳转 鳞翅目
+
+function tuJian_insect_linChiMu() {
+    top.location.replace("./tuJian_insect_linChiMu.html")
+}//页面跳转 鳞翅目
+
+function tuJian_insect_banChiMu() {
+    top.location.replace("./tuJian_insect_banChiMu.html")
+}//页面跳转 半翅目
+
+function tuJian_insect_other() {
+    top.location.replace("./tuJian_insect_other.html")
+}//页面跳转 其他目
 
 function tujian_niaoLei() {
-    window, location.href = ("./index_tujian_niaoLei.html")
+    top.location.replace("./index_tujian_niaoLei.html")
 }//页面跳转 鸟类图鉴
 
 function tujian_zhiWu() {
-    window, location.href = ("./index_tujian_zhiWu.html")
+    top.location.replace("./index_tujian_zhiWu.html")
 }//页面跳转 植物图鉴
 
 function index_fenLei() {
-    window.location.href = ("./index_fenLei.html")
+    top.location.replace("./index_fenLei.html")
 }//页面跳转 分类知识
 
+function index_duShuBiJi() {
+    top.location.replace("./index_duShuBiJi.html")
+}//页面跳转 学习笔记
+
+
+function index_other() {
+    top.location.replace("./index_other.html")
+}//其他页面
+
 function bianChengBiJi() {
-    window.location.href = ("./index_bianChengBiJi.html")
+    window.open("./index_bianChengBiJi.html")
 }//页面跳转 编程笔记
 function drawing001() {
     window.open("./index_drawing_001.html")
 }
 function morpho() {
-    window.location.href = "./breed_lc_morpho.html"
+    top.location.replace("./breed_lc_morpho.html")
 }//大蓝闪饲养
 
 function jinBanDie() {
-    window.location.href = "./breed_lc_jinBanDie.html"
+    top.location.replace("./breed_lc_jinBanDie.html")
 }//金斑蝶饲养
 
 function hongYiQingLong() {
-    window.open("./breed_xiu.html")
+    top.location.replace("./breed_xiu.html")
 }//红翼青龙
 
-function index_duShuBiJi() {
-    window.location.href = "./index_duShuBiJi.html"
-}//读书笔记页面
 
-
-function index_other() {
-    window.location.href = "./index_other.html"
-}//其他页面
 
 function index_price() {
     window.open("./index_price.html")
@@ -67,34 +81,38 @@ function index_list() {
 }//各类名录
 
 function zhuanli() {
-    window.open("./other_zhuanli.html")
+    top.location.replace("./other_zhuanli.html")
 }//专利页面
 function key() {
-    window.open("./other_key.html")
+    top.location.replace("./other_key.html")
 }//快捷键页面
 
 function szds() {
-    window.open("./ebook_szds.html")
+    top.location.replace("./ebook_szds.html")
 }//数字雕塑技法与3D打印
 function Blender01() {
-    window.open("./ebook_Blender01.html")
+    top.location.replace("./ebook_Blender01.html")
 }//Blender权威指南
 
 function js01() {
-    window.open("./ebook_js01.html")
+    top.location.replace("./ebook_js01.html")
 }//JavaScript DOM 编程艺术
 
 function js02() {
-    window.open("./ebook_js02.html")
+    top.location.replace("./ebook_js02.html")
 }//锋利的jquery
 
 function js03() {
-    window.open("./ebook_js03.html")
+    top.location.replace("./ebook_js03.html")
 }//条路编程
 
 function stable() {
-    window.open("./ebook_stable_diffusion.html")
+    top.location.replace("./ebook_stable_diffusion.html")
 }//stable diffusion
+
+function penAndInk() {
+    top.location.replace("./ebook_draw_penAndInk.html")
+}//How to draw in pen and ink
 
 
 function tang01() {
@@ -124,16 +142,33 @@ function jd01() {
     window.open("./tujian_qiaoChiMu_jd01.html")
 }//彩虹锥齿吉丁
 
-
-
-
-
-
+function defh() {
+    window.open("./tujian_zhiChiMu_defh.html")
+}//短额负蝗
+function hjlvh() {
+    window.open("./tujian_zhiChiMu_hjlvh.html")
+}//花胫绿纹蝗
+function edwye() {
+    window.open("./tujia_linChiMu_edwye.html")
+}//二点委夜蛾
+function hzwm() {
+    window.open("./tujia_linChiMu_hzwm.html")
+}//灰直纹螟
+function shzce() {
+    window.open("./tujia_linChiMu_shzce.html")
+}//上海枝尺蛾
 
 function low_01() {
-    window.open("./other_low_01.html")
+    top.location.replace("./other_low_01.html")
 }//入侵物种相关法律条文
 
+function comp_01() {
+    top.location.replace("./other_computer.html")
+}//电脑网络相关
+
+function tiao_xiJinGui() {
+    location.href = "#tiao_xiJinGui"
+}
 
 
 
